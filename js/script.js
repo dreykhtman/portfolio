9c02c4799c551386a89078c6daef6e37e20bedef
Dima <myuziw8s1t@gmail.com>
2021-02-24 21:46:50 -0500

    SVG arrows work!

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
 console.log('script.js is connected...');
+
+const from = document.getElementById('from');
+const to = document.getElementById('to');
+const svgFrom = document.getElementById('svg-from');
+const svgTo = document.getElementById('svg-to');
+
+svgFrom.setAttribute(
+  'd',
+  `M ${from.getBoundingClientRect().x} ${
+    from.getBoundingClientRect().y
+  } Q 400 700 ${to.getBoundingClientRect().x + 80} ${
+    to.getBoundingClientRect().bottom - 60
+  }`
+);
+
+document.addEventListener('scroll', function () {
+  svgFrom.setAttribute(
+    'd',
+    `M ${from.getBoundingClientRect().x} ${
+      from.getBoundingClientRect().y
+    } Q 400 700 ${to.getBoundingClientRect().x + 80} ${
+      to.getBoundingClientRect().bottom - 60
+    }`
+  );
+});
+
+window.addEventListener('resize', function () {
+  svgFrom.setAttribute(
+    'd',
+    `M ${from.getBoundingClientRect().x} ${
+      from.getBoundingClientRect().y
+    } Q 400 700 ${to.getBoundingClientRect().x + 80} ${
+      to.getBoundingClientRect().bottom - 60
+    }`
+  );
+});
