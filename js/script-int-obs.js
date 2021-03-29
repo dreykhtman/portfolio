3b277abe6dfea6497f084fb1d345e1ecc90d1b7f
Dima <myuziw8s1t@gmail.com>
2021-03-29 17:31:56 -0400

    Restructure js to point arrows at screenshots, not at their containers

--- js/script-int-obs.js
+++ js/script-int-obs.js
@@ -1,17 +1,28 @@
-'use strict';
-
-const screenshots = document.querySelectorAll('.screenshot');
+const screenshotContainers = document.querySelectorAll('.screenshot-container');
 const descriptions = document.querySelectorAll('.description');
-// const arrows = document.querySelectorAll('.arrow');
-
-// arrows.forEach((arrow) => {
-//   console.log(arrow.id);
-// });
 
 const descriptionList = {};
 
+// Create the descriptionList object, where the keys are names of the description IDs ("description-cotillion", "description-portfolio", etc), and the values are arrays of elements to which the class "hidden" is removed or added.
+
+// Simplified example:
+/*
+{
+  description-cotillion: [
+    div#description-cotillion.description.hidden,
+    path#svg-cotillion-nav.arrow.arrow-cotillion.hidden,
+    path#svg-cotillion-sticky.arrow.arrow-cotillion.hidden,
+  ],
+  description-portfolio: [
+    div#description-portfolio.description,
+    path#svg-portfolio-svg.arrow.arrow-portfolio,
+  ]
+}
+*/
+
 descriptions.forEach((description) => {
   const arrows = document.querySelectorAll(
+    // In substring(12), 12 is the length of "description-"
     `.arrow-${description.id.substring(12)}`
   );
 
@@ -27,15 +38,16 @@ const options = {
 const observerCallback = (entries, observer) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      descriptionList[`description-${entry.target.id.substring(11)}`].forEach(
+      // In substring(21), 21 is the length of "screenshot-container-"
+      descriptionList[`description-${entry.target.id.substring(21)}`].forEach(
         (description) => {
-          description.classList.add('hidden');
+          description.classList.add('hidden', 'back-color');
         }
       );
     } else {
-      descriptionList[`description-${entry.target.id.substring(11)}`].forEach(
+      descriptionList[`description-${entry.target.id.substring(21)}`].forEach(
         (description) => {
-          description.classList.remove('hidden');
+          description.classList.remove('hidden', 'back-color');
         }
       );
     }
@@ -44,6 +56,6 @@ const observerCallback = (entries, observer) => {
 
 const observer = new IntersectionObserver(observerCallback, options);
 
-screenshots.forEach((screenshot) => {
-  observer.observe(screenshot);
+screenshotContainers.forEach((container) => {
+  observer.observe(container);
 });
