5b14a6129ae1a38945ce1db2428d177b75bc606d
Dima <myuziw8s1t@gmail.com>
2021-07-29 19:24:25 -0400

    Build footer

--- js/helper-functions.js
+++ js/helper-functions.js
@@ -1,3 +1,22 @@
 // Returns "nav" from "svg-cotillion--nav".
 // eslint-disable-next-line no-unused-vars
 const getIdentifier = (str) => str.split('--').pop();
+
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+  });
+};
+
+document.getElementById('btn-scroll').addEventListener('click', scrollToTop);
+
+const btnEmail = document.getElementById('btn-email');
+
+const showEmail = () => {
+  const contact = document.getElementById('contact');
+  const html = '<a href="mailto:dreykhtman@gmail.com">dreykhtman@gmail.com</a>';
+  contact.insertAdjacentHTML('beforeend', html);
+  btnEmail.hidden = true;
+};
+
+btnEmail.addEventListener('click', showEmail);
